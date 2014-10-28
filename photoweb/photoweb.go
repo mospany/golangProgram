c30b639b730913a4a0550ed24f71dd0c5fa43ddd
= <=>
2014-10-28 23:10:46 +0800

    显示图像成功

--- photoweb/photoweb.go
+++ photoweb/photoweb.go
@@ -4,9 +4,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
-func uploadHander(w http.ResponseWriter, r *http.Request) {
+const (
+	UPLOAD_DIR = "./uploads"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		io.WriteString(w, "<html><form method=\"POST\" action=\"/upload\" "+
 			" enctype=\"multipart/form-data\">"+
@@ -14,10 +19,41 @@ func uploadHander(w http.ResponseWriter, r *http.Request) {
 			"<input type=\"submit\" value=\"Upload\" />"+
 			"</form></html>")
 	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		filename := h.Filename
+		defer f.Close()
+
+		t, err := os.Create(UPLOAD_DIR + "/" + filename)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer t.Close()
+
+		if _, err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
 }
 
 func main() {
-	http.HandleFunc("/upload", uploadHander)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/view", viewHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err.Error())
