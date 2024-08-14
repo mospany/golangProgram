60fe0f1d58af3cb7a3e535bc0f3bf4b6f64a0a86
mospany <moshengping@163.com>
2024-08-14 16:32:35 +0800

    添加twoSum

--- gpt100/1.twoSum/twoSum.go
+++ gpt100/1.twoSum/twoSum.go
@@ -1,5 +1,10 @@
 package __twoSum
 
+/*
+1. 两数之和
+描述: 给定⼀个整数数组和⼀个⽬标值，找出数组中和为⽬标值的两个数的索引。
+思路: 使⽤哈希表记录数字及其索引，遍历数组查找。
+*/
 func twoSum(nums []int, target int) []int {
 	indexMap := make(map[int]int)
 	for idx, num := range nums {
